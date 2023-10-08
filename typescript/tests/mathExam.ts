c64d5b843d2692035a1eb544b713c3b764c66888
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-08 22:45:37 +0700

    add math unit test

---
+++ typescript/tests/mathExam.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from 'assert';
+import { Operators, arithmetic } from '../maths.ts';
+
+Deno.test("Calculating exam", () => {
+	assertEquals(arithmetic(3, 478, Operators.Plus), 481);
+	assertEquals(arithmetic(20, 46, Operators.Minus), -26);
+	assertEquals(arithmetic(27, 478, Operators.Multiply), 12_906)
+})
+
+Deno.test("Division exam", () => {
+	assertEquals(arithmetic(20, 4, Operators.Divide), 5);
+	assertEquals(arithmetic(150, 2, Operators.Divide), 75);
+	assertEquals(arithmetic(2048, 512, Operators.Divide), 4);
+	assertEquals(arithmetic(2, 10, Operators.Divide), 0.2);
+	assertEquals(arithmetic(16, 3, Operators.Divide), 5.333333333333333);
+	assertEquals(arithmetic(5, 0, Operators.Divide), '[Indivisible by 0]');
+})
+
+Deno.test("Trigonometry exam", () => {
+	// assertEquals(trigonometry(TrigTypes.Sine, 1), 90);
+})
