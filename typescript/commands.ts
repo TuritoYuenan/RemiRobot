f0cc1a2e24019400116e59bc690630ce9bf0f781
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-08 20:26:18 +0700

    add spell and calculate command

--- typescript/commands.ts
+++ typescript/commands.ts
@@ -1,17 +1,51 @@
 import { SlashCommandPartial } from 'harmony';
 
+export enum Operators {
+	Plus = '+',
+	Minus = '-',
+	Multiply = '*',
+	Divide = '/',
+}
+
 export const commands: SlashCommandPartial[] = [
 	{
 		name: 'ping',
 		description: 'Return Article 18 in the UDHR',
 	},
 	{
-		name: "spell",
-		description: "Spell out the inputed word",
+		name: 'spell',
+		description: 'Spell out the inputed word',
 		options: [{
-			name: "input",
-			description: "Word to spell",
-			type: "STRING"
+			name: 'input',
+			description: 'Word to spell',
+			type: 'STRING',
+			required: true,
 		}],
-	}
+	},
+	{
+		name: 'calculate',
+		description: 'Do basic calculation',
+		options: [{
+			name: 'num1',
+			description: 'First number',
+			type: 'NUMBER',
+			required: true,
+		}, {
+			name: 'num2',
+			description: 'Second number',
+			type: 'NUMBER',
+			required: true,
+		}, {
+			name: 'operator',
+			description: 'Operator',
+			type: 'STRING',
+			required: true,
+			choices: [
+				{ name: 'Add', value: Operators.Plus },
+				{ name: 'Subtract', value: Operators.Minus },
+				{ name: 'Multiply', value: Operators.Multiply },
+				{ name: 'Divide', value: Operators.Divide },
+			],
+		}],
+	},
 ];
--- typescript/mod.ts
+++ typescript/mod.ts
@@ -31,8 +31,45 @@ class RemiBot extends Client {
 
 	@slash('spell')
 	spell(i: Interaction) {
-		const spelt: EmbedPayload = {}
-		i.respond({ embeds: [spelt] })
+		if ('options' in i.data!) {
+			const input = i.data.options.find((e) => e.name == 'input');
+			const word: string = input?.value;
+			const spelt = word.split('').toString();
+			i.respond({ content: spelt });
+		}
+	}
+
+	@slash('calculate')
+	calculate(i: Interaction) {
+		if ('options' in i.data!) {
+			const num1 = i.data.options.find((e) => e.name == 'num1')?.value;
+			const num2 = i.data.options.find((e) => e.name == 'num2')?.value;
+			const oprd = i.data.options.find((e) => e.name == 'operator')?.value;
+			let result = 0;
+
+			switch (oprd) {
+				case Operators.Plus:
+					result = num1 + num2;
+					break;
+				case Operators.Minus:
+					result = num1 - num2;
+					break;
+				case Operators.Multiply:
+					result = num1 * num2;
+					break;
+				case Operators.Divide:
+					result = num1 / num2;
+					break;
+			}
+
+			const response: EmbedPayload = {
+				title: `The result is ${result}`,
+				description: `Calculate: ${num1} ${oprd} ${num2}`,
+				color: 0xFFCC00,
+			};
+
+			i.respond({ embeds: [response] });
+		}
 	}
 }
 
